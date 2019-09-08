b008c801edbe86a8a5938b4403dacc539aaef86b
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-08 18:01:19 +0900

    keyboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,37 @@
-import React from "react"
+import React, { createContext, useContext } from "react"
 import { render } from "react-dom"
+import { Synth } from "tone"
 
+const ToneContext = createContext({
+  synth: new Synth().toMaster()
+})
+
+const TriggerAttackRelease = ({ note }) => {
+  const { synth } = useContext(ToneContext)
+  return (
+    <div>
+      <button
+        onClick={() => {
+          synth.triggerAttackRelease(note, "8n")
+        }}
+      >
+        {note}
+      </button>
+    </div>
+  )
+}
 const App = () => {
-  return <div>hello</div>
+  return (
+    <div>
+      <TriggerAttackRelease note="C4" />
+      <TriggerAttackRelease note="D4" />
+      <TriggerAttackRelease note="E4" />
+      <TriggerAttackRelease note="F4" />
+      <TriggerAttackRelease note="G4" />
+      <TriggerAttackRelease note="A4" />
+      <TriggerAttackRelease note="B4" />
+    </div>
+  )
 }
 
 render(<App />, document.querySelector("#container"))
