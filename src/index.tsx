7da0dd641e42d1be205d40dbb4d80fa528a83fb6
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-08 18:04:54 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -22,13 +22,20 @@ const TriggerAttackRelease = ({ note }) => {
 const App = () => {
   return (
     <div>
-      <TriggerAttackRelease note="C4" />
-      <TriggerAttackRelease note="D4" />
-      <TriggerAttackRelease note="E4" />
-      <TriggerAttackRelease note="F4" />
-      <TriggerAttackRelease note="G4" />
-      <TriggerAttackRelease note="A4" />
-      <TriggerAttackRelease note="B4" />
+      <div>
+        <a href="https://github.com/terrierscript/example-tone" target="_blank">
+          Source
+        </a>
+      </div>
+      <div>
+        <TriggerAttackRelease note="C4" />
+        <TriggerAttackRelease note="D4" />
+        <TriggerAttackRelease note="E4" />
+        <TriggerAttackRelease note="F4" />
+        <TriggerAttackRelease note="G4" />
+        <TriggerAttackRelease note="A4" />
+        <TriggerAttackRelease note="B4" />
+      </div>
     </div>
   )
 }
